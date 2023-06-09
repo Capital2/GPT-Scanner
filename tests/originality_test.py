d79663d97992b659342bf7b40e73738813364a89
Fedi Takeli <fedi.takeli1@gmail.com>
2023-06-09 13:55:43 +0100

    init tests

---
+++ tests/originality_test.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest import mock
+from originality import *
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'http://someurl.com/test.json':
+        return MockResponse({"key1": "value1"}, 200)
+    elif args[0] == 'http://someotherurl.com/anothertest.json':
+        return MockResponse({"key2": "value2"}, 200)
+
+def mocked_requests_post(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'http://someurl.com/test.json':
+        return MockResponse({"key1": "value1"}, 200)
+    elif args[0] == 'http://someotherurl.com/anothertest.json':
+        return MockResponse({"key2": "value2"}, 200)
+    elif kwargs["somearg"] == "something":
+        pass
+
+    return MockResponse(None, 404)
+
+class OriginalityTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
