c9b31f0981dee1dfb2572b3a2149fd4e3124de76
BlueGob <moslemgannoun@gmail.com>
2023-08-30 08:12:28 +0100

    bearer token fixed
    after some debug i found that the bearer token wasn't necessary

--- zeroGPT/__init__.py
+++ zeroGPT/__init__.py
@@ -1,6 +1,4 @@
 import requests
-import numpy as np
-np.random
 def zeroGPTVerdict(text:str)->dict:
     """
     this function return ``dict`` that contains:
@@ -15,11 +13,11 @@ def zeroGPTVerdict(text:str)->dict:
     data = {
         "input_text": text
     }
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
+    headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
     "Accept": "application/json, text/plain, */*",
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate, br",
-    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIzNTQiLCJyb2xlIjoiMyIsInNhbGF0YV9lbmdpbmUiOiIyLjciLCJjb3N0X3Blcl90aG91c2FuZCI6IjAuMDM0IiwibnVtYmVyX29mX2NoYXJhY3RlcnMiOiI1MDAwMCIsIm51bWJlcl9vZl9maWxlcyI6IjQwLjAiLCJleHAiOjE3MjQ0MTYzNjZ9.B7HlbzROE3Kb-VJCXnPg0u3vLC2FvCVrJxYtnY9ue8ShNxnmK6YMwidv4NzyBPiqo4L5PjE4PAXLpPVZWz-T0rSVwNn-0lbEw-oEPtRqWZ-j2KHpihf7KY-zs5mYjA4Vq4X7WUbnwsPxl56xNWUVsjGKGzpWPd9ag5MA9PcjTus",
     "Content-Type": "application/json",
     "content-length": str(len(text)),
     "Origin": "https://www.zerogpt.com" 
@@ -28,7 +26,7 @@ def zeroGPTVerdict(text:str)->dict:
     
     response =  response.json()
 
-    result ={
+    result = {
         "ai_percentage":response["data"]["fakePercentage"],
         "suspected_text":response["data"]["h"],
         "additional_feedback":response["data"]["additional_feedback"]
