d6ddb5ddd006fb2d827c241e1f99ef23edcacd10
Fedi Takeli <fedi.takeli1@gmail.com>
2023-09-06 22:56:16 +0100

    logging

--- plagiarism/__init__.py
+++ plagiarism/__init__.py
@@ -1,8 +1,12 @@
 import requests
 import urllib.parse as parser
+import logging
+
+LOG = logging.getLogger(__name__)
+
 def plagiarismChecker(data:str)->list[dict]:
     url = "https://seomagnifier.com/online-plagiarism-checker/check"
-
+    ultraSuperSecureSecretToken="ggggggg"
     headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
     "Accept": "application/json, text/javascript, */*; q=0.01",
@@ -11,8 +15,11 @@ def plagiarismChecker(data:str)->list[dict]:
     }
     data = data.replace(".","_sysbreak_[--atozbreak--]")
     
-    body = "queries="+parser.quote_plus(data)+"&SecureToken=ggggggg"
+    body = "queries="+parser.quote_plus(data)+f"&SecureToken={ultraSuperSecureSecretToken}"
+    LOG.debug(f"post request body: {body}")
     response = requests.post(url,  headers=headers, data=body)
+    LOG.info(f"POST request returned with {response}")
+    LOG.debug(f"response body: {response.text}")
     response = response.json()['details']
     list_of_queries = []
     
@@ -22,4 +29,5 @@ def plagiarismChecker(data:str)->list[dict]:
         else:
             query = {"text":item["query"],"link":"None"}
         list_of_queries.append(query)
+    LOG.debug(f"plag checker returned with: {list_of_queries}")
     return list_of_queries
