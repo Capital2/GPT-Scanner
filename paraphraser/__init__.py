f7a1eb7485126b737e9d5b8c817fe091c194ad44
Moslem Gannoun <moslemgannoun@gmail.com>
2023-10-06 20:29:37 +0100

    fixed cookie (#13)
    hotfix: paraphraser is checking session cookies.
    
    ---------
    
    Co-authored-by: Capital2 <fedi.takeli1@gmail.com>

--- paraphraser/__init__.py
+++ paraphraser/__init__.py
@@ -11,20 +11,22 @@ def paraphrase(data:str,lang:str,mode:str)->str:
     """
 
     url = "https://www.paraphraser.io/frontend/rewriteArticleBeta"
-
-    headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
+    
+    session = requests.Session()
+    session.head("https://www.paraphraser.io/")
+    session.headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
     "Accept": "application/json, text/javascript, */*; q=0.01",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "X-Requested-With": "XMLHttpRequest",
     "Origin": "https://www.paraphraser.io" ,
-    "Referer": "https://www.paraphraser.io/fr/outil-de-paraphrase"
+    "Referer": "https://www.paraphraser.io/"
     }
 
     body = "data="+parser.quote_plus(data)+'+&mode='+mode+'&lang='+lang
 
     LOG.info(f"post request body: {body}")
-    response = requests.post(url,  headers=headers, data=body)
+    response = session.post(url, data=body)
     LOG.info(f"POST request returned with {response}")
     LOG.info(f"response body: {response.text}")
 
